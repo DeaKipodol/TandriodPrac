3cfe855930fe6b826354f35deb16b07d60ae98ab
Your Name <yang10502@naver.com>
2024-08-14 22:41:20 +0900

    김경섭 강의내용 3

--- app/src/main/java/com/chobo/practice/MainActivity.kt
+++ app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tv: TextView =findViewById(R.id.tv_hello)
+        val btn: Button =findViewById(R.id.btn_kor)
+        btn.setOnClickListener{
+            tv.text="안녕"
+        }
         }
     }
 }
