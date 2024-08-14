e56d6ed922f301cc4a4f3fb7c668b918cb95359b
Kimdaeki <podol0416@gmail.com>
2024-08-14 23:13:29 +0900

    타이머기능과 버튼누르면 목표시간 맟추기기능 추가

--- app/src/main/java/com/chobo/practice/MainActivity.kt
+++ app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.chobo.practice
+package com.chobo.practiced
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 import java.util.Timer
-import kotlin.concurrent.timer
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,24 +16,41 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var timerTask : Timer? = null
+        var sec: Int = 0
+        val tv: TextView = findViewById(R.id.tv_random)
+        val tv_t: TextView = findViewById(R.id.tv_timer)
+        val tv_p:TextView = findViewById(R.id.tv_point)
+        val btn: Button = findViewById(R.id.btn_main)
         var isRunning = false
-        var sec : Int = 0
-        val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
+        var timerTask: Timer? = null
 
-        btn.setOnClickListener{
+            val random_box = Random()
+            val num = random_box.nextInt(1001)
+            tv.text =((num.toFloat())/100).toString()
+
+
+        btn.setOnClickListener {
             isRunning = !isRunning
             if (isRunning == true) {
-                timerTask = kotlin.concurrent.timer(period = 1000) {
+                timerTask = kotlin.concurrent.timer(period = 10) {
                     sec++
                     runOnUiThread {
-                        tv.text = sec.toString()
+                        tv_t.text = (sec.toFloat() / 100).toString()
                     }
                 }
             } else {
                 timerTask?.cancel()
+               val point= (kotlin.math.abs(sec - num)).toFloat()/100
+                tv_p.text=point.toString()
             }
+
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tv_point)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
